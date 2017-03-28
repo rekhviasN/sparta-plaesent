9315f0069146dfd6b36f0c79a0f86f9c5be58608
Nino <rekhviasnino@gmail.com>
2017-03-27 23:47:30 -0400

    updated styling

--- app/js/components/ArticleContainer.js
+++ app/js/components/ArticleContainer.js
@@ -8,28 +8,28 @@ const data = [
 	image: "js/public/1.png"
 },
 {
-	date:"02/02/2017",
-	title: "Sharp bureaux sleeply K-pop carefully curated.",
-	author: "Lorem Ipsum",
-	image: "js/public/2.png"
-},
-{
-	date: "01/27/2017",
-	title: "St Moritz uniforms Beams.",
-	author: "Lorem Ipsum",
-	image: "js/public/3.png"
-},
-{
 	date: "01/21/2017",
 	title: "Esse airport veniam ryokan soft power.",
 	author: "Lorem Ipsum",
 	image: "js/public/4.png"
+},
+{
+	date:"02/02/2017",
+	title: "Sharp bureaux sleeply K-pop carefully curated.",
+	author: "Lorem Ipsum",
+	image: "js/public/2.png"
 },
 {	
 	date: "01/18/2017",
 	title: "K-pop extraordinary.",
 	author: "Lorem Ipsum",
 	image: "js/public/5.png"
+},
+{
+	date: "01/27/2017",
+	title: "St Moritz uniforms Beams.",
+	author: "Lorem Ipsum",
+	image: "js/public/3.png"
 },
 {
 	date: "01/12/2017",
